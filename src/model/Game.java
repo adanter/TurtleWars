106730e3d8de5c0ba587bb1b05d202d1fc39419a
adanterr <adanterr@bitbucket.com>
2017-04-14 22:40:17 -0500

    Still adding things to Git

---
+++ src/model/Game.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Adante on 4/14/2017.
+ */
+public class Game {
+    private int numPlayers;
+    private ArrayList<Turtle> turtles = new ArrayList<Turtle>();
+}
