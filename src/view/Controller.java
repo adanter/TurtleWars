84770b39c7b3fda2806c06c7c10c57ba25d44852
Adam Klein <kleina3@carleton.edu>
2017-04-14 22:56:37 -0500

    bare bones fxml

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,4 +1,13 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
 public class Controller {
+    @FXML
+    HBox statusBar;
+
+    public void menuButtonPressed() {
+        System.out.println("Pressed Button");
+    }
 }
