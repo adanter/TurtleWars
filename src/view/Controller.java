09365236fcefef5c5909d5399f7eed2561da90ba
Adam Klein <kleina3@carleton.edu>
2017-04-15 00:04:42 -0500

    added key event listening

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,13 +1,29 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 
 public class Controller {
     @FXML
     HBox statusBar;
 
-    public void menuButtonPressed() {
+    public void menuButtonClicked() {
         System.out.println("Pressed Button");
+    }
+
+    public void keyListener(KeyEvent event){
+        switch (event.getCode()){
+            case UP : System.out.println("UP");
+            break;
+            case DOWN: System.out.println("DOWN");
+            break;
+            case LEFT : System.out.println("LEFT");
+            break;
+            case RIGHT : System.out.println("RIGHT");
+            break;
+        }
+
     }
 }
