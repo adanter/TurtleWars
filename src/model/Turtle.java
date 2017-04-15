e98412a0adb73fcfc7de38c3005aa96009d8f647
Kiya Govek <kiyagovek@eduroam-173-27.dyn.carleton.edu>
2017-04-15 00:49:45 -0500

    objectvector in turtle

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -13,24 +13,30 @@ public class Turtle {
     int size;
     int color;
     int player;
-    private double[] pos = new double[2];
-    private double[] velocity = new double[2];
+    private ObjectVector position = new ObjectVector(0,0);
+    private ObjectVector velocity = new ObjectVector(0,0);
     private double direction_facing;
-    private double rotate_vel = 0;
+    private double rotateVel = 0;
 
     public void changeVel(int direction){
-        // set velocity to vector(direction_facing) times direction
+        ObjectVector nextVel = new ObjectVector(0,0);
+        nextVel.setAngle(direction_facing);
+        nextVel = nextVel.getUnitVector();
+        nextVel.scalarMultiply(direction_facing);
+        velocity = nextVel;
     }
     // side: -1 = left, 1 = right
     public void turn(int side) {
-        rotate_vel = side;
+        rotateVel = side*0.1;
     }
 
-    public void move(double timeStep) {
-        //pos += velocity;
-        //direction_facing += rotate_vel
+    public void update(double timeStep) {
+        velocity.scalarMultiply(timeStep);
+        position.addVector(velocity);
+        ObjectVector angleVector = new ObjectVector(0,0);
+        direction_facing += rotateVel;
     }
     public void shoot(){}
-    public double[] getPos() {return pos;}
-    public double[] getVelocity() {return velocity;}
+    public ObjectVector getPosition() {return position;}
+    public ObjectVector getVelocity() {return velocity;}
 }
