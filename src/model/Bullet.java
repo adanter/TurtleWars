fd561e38976c2e62cdef86f6f940f15bc1980d9f
adanterr <adanterr@bitbucket.com>
2017-04-15 00:29:35 -0500

    Created a class for vector operations

---
+++ src/model/Bullet.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Adante on 4/14/2017.
+ */
+public class Bullet {
+
+}
--- src/view/Controller.java
+++ src/view/Controller.java
@@ -2,12 +2,35 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
+import model.ComputerPlayer;
+import model.HumanPlayer;
+import model.Player;
+import model.Turtle;
+
+import java.util.Map;
 
 public class Controller {
     @FXML
     HBox statusBar;
+    private int numPlayers;
+    private Map<Player, Turtle> playerTurtleMap;
 
     public void menuButtonPressed() {
         System.out.println("Pressed Button");
     }
+
+    /**
+     * Method for creating a new player and a corresponding turtle
+     */
+    private void addPlayer(boolean isHuman) {
+        Player newPlayer;
+        if (isHuman) {
+            newPlayer = new HumanPlayer();
+        } else {
+            newPlayer = new ComputerPlayer();
+        }
+        Turtle newTurtle = new Turtle();
+        playerTurtleMap.put(newPlayer, newTurtle);
+    }
+
 }
